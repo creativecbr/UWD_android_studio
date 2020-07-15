bbce32e95dcadc73e32da83e3231cf7846ef5d81
creativecbr <lesniakcbr@gmail.com>
2020-07-15 12:32:18 +0200

    Feature-1: Main menu
    Added:
    -Main menu logic
    -Design from AdobeXD project
    -About author activity based on fragments
    -How to Play activity based on fragments
    -Back buttons in fragments
    
    Modified:
    -Value files: dimens, strings, styles

--- app/src/main/java/lesniewski/pawel/uwd_android_studio/MainActivity.kt
+++ app/src/main/java/lesniewski/pawel/uwd_android_studio/MainActivity.kt
@@ -2,10 +2,28 @@ package lesniewski.pawel.uwd_android_studio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IFragmentChanger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ChangeFragment(supportFragmentManager, R.id.fragmentMenu, GameMainMenu())
+
+    }
+
+
+
+}
+
+interface IFragmentChanger{
+
+    fun ChangeFragment(fragmentManager: FragmentManager, fragment: Int, secondFragment: Fragment) {
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(fragment, secondFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
