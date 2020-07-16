8ba9afb5c041dd81e1033e5dea2d77360d55439b
creativecbr <lesniakcbr@gmail.com>
2020-07-16 17:19:37 +0200

    Added:
    -Turning Bluetooth on client, and checking is on
    -New activity to show available rooms

--- app/src/main/java/lesniewski/pawel/uwd_android_studio/GameRoomChoosing.kt
+++ app/src/main/java/lesniewski/pawel/uwd_android_studio/GameRoomChoosing.kt
@@ -1,6 +1,7 @@
 package lesniewski.pawel.uwd_android_studio
 
 
+import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,10 +9,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 
 class GameRoomChoosing : Fragment(), IFragmentChanger {
+
+    private val REQUEST_CODE_ENABLE_BT: Int = 1
+    lateinit var bAdapter:BluetoothAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,15 +31,55 @@ class GameRoomChoosing : Fragment(), IFragmentChanger {
             ChangeFragment(activity!!.supportFragmentManager, R.id.fragmentMenu, GameMainMenu())
         }
 
-        val app_layer = v.findViewById(R.id.createRoomLayout) as LinearLayout
-        app_layer.setOnClickListener{
+        val create_layer = v.findViewById(R.id.createRoomLayout) as LinearLayout
+        create_layer.setOnClickListener{
             val intent = Intent(activity, CreateRoom::class.java)
             startActivity(intent)
+        }
+
+
+        val join_layer = v.findViewById(R.id.joinRoomLayout) as LinearLayout
+        join_layer.setOnClickListener{
+
+            checkIsBluetoothOnAndGoToClientActivity()
         }
 
         return v
     }
 
+    private fun checkIsBluetoothOnAndGoToClientActivity()
+    {
+        bAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(!bAdapter.isEnabled)
+        {
+            val blInt = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(blInt, REQUEST_CODE_ENABLE_BT)
+        }
+        else
+        {
+            val intent = Intent(activity, JoinToRoom::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data:Intent?) {
+
+        when(requestCode)
+        {
+            REQUEST_CODE_ENABLE_BT ->
+                if (resultCode != AppCompatActivity.RESULT_OK)
+                {
+                    Toast.makeText(this.context, "Najpierw musisz zezwolić na uruchomienie bluetooth!", Toast.LENGTH_LONG).show()
+                }
+                else
+                {
+                    val intent = Intent(activity, JoinToRoom::class.java)
+                    startActivity(intent)
+                }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
 
 
---
+++ app/src/main/java/lesniewski/pawel/uwd_android_studio/JoinToRoom.kt
@@ -0,0 +1,11 @@
+package lesniewski.pawel.uwd_android_studio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinToRoom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_to_room)
+    }
+}
