58dc8c43ef565d117668c5a2066dc89922240bf6
creativecbr <lesniakcbr@gmail.com>
2020-07-23 18:45:14 +0200

    Add: -information about max players in room -information about connected players to room Modified: -serverWaiting room is prepared for receiving data -string values xml

--- app/src/main/java/lesniewski/pawel/uwd_android_studio/ServerWaitingForPlayers.kt
+++ app/src/main/java/lesniewski/pawel/uwd_android_studio/ServerWaitingForPlayers.kt
@@ -1,42 +1,55 @@
 package lesniewski.pawel.uwd_android_studio
 
-import android.R.string
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_server_waiting_for_players.*
+import java.lang.Exception
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class ServerWaitingForPlayers : AppCompatActivity()
 {
+    private var ROOM_NAME = "---"
+    private var PLAYER_LIMIT = 6
+    private val APP_UUID = UUID.fromString("3b4c7719-3738-4234-94a3-22d72dbb8a74")
     private val REQUEST_CODE_ENABLE_BT: Int = 1
-    private var bluetoothOn = false
+
+    val STATE_LISTENING = 1
+    val STATE_CONNECTING = 2
+    val STATE_CONNECTED = 3
+    val STATE_CONNECTION_FAILED = 4
+    val STATE_MESSAGE_RECEIVED = 5
+
+
+    lateinit var refreshButton: Button
+    lateinit var nextButton: Button
+    lateinit var backButton: Button
+    lateinit var devicesArray: ArrayList<BluetoothDevice>
+    lateinit var pairedList: ListView
     lateinit var bAdapter: BluetoothAdapter
     lateinit var strings: ArrayList<String>
+    lateinit var adapter: ArrayAdapter<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_server_waiting_for_players)
-        showRoomName()
 
-        bAdapter = BluetoothAdapter.getDefaultAdapter()
-        val nextButton = findViewById<Button>(R.id.nextButton)
-        val refreshButton = findViewById<Button>(R.id.refreshButton)
+        findAllViews()
+        showRoomNameAndPlayerLimit()
+        limitPlayersAmount(6)
+        connectListToSockets()
 
-        strings = getBoundedDevices()
-
-        var pairedList = findViewById<ListView>(R.id.pairedDevicesList)
-
-        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_list_item_1,
-            strings
-        )
-        pairedList.adapter = adapter
-
+       /*
 
         refreshButton.setOnClickListener{
             var tmp = getBoundedDevices()
@@ -45,36 +58,80 @@ class ServerWaitingForPlayers : AppCompatActivity()
                     strings.add(device)
 
             adapter.notifyDataSetChanged()
-        }
+        }*/
     }
 
-    private fun getBoundedDevices(): ArrayList<String> {
+    private fun connectListToSockets() {
+        adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            strings
+        )
+        pairedList.adapter = adapter
+    }
 
-        var bt = bAdapter.bondedDevices
-        val strings: ArrayList<String> = ArrayList<String>()
-
-        if(bt.size > 0)
-        {
-            for (device in bt) {
-                strings.add(device.name)
-            }
-        }
-        else
-        {
-            nextButton.isEnabled = false
-        }
-
-        return strings
+    private fun limitPlayersAmount(limit: Int) {
+        PLAYER_LIMIT = limit
     }
 
 
-    fun showRoomName()
+    private fun findAllViews() {
+        bAdapter = BluetoothAdapter.getDefaultAdapter()
+        nextButton = findViewById<Button>(R.id.nextButton)
+        refreshButton = findViewById<Button>(R.id.refreshButton)
+        pairedList = findViewById<ListView>(R.id.pairedDevicesList)
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    fun showRoomNameAndPlayerLimit()
     {
         val extras = intent.extras
         var name = ""
         if (extras != null) {
             name = extras.getString("roomName").toString()
         }
+        ROOM_NAME = name
+
         showRoomName.text=name
+        maxPlayersInfo.text= resources.getString(R.string.maxPlayersInfo) + PLAYER_LIMIT.toString()
+    }
+
+    inner class CollectingPlayers(var serverSocket: BluetoothServerSocket): Thread()
+    {
+        init{
+            try{
+                serverSocket = bAdapter.listenUsingRfcommWithServiceRecord(ROOM_NAME, APP_UUID)
+            }
+            catch (e: Exception)
+            {
+                println("Server socket listening set error.")
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun run() {
+            var sockets: ArrayList<BluetoothSocket> = ArrayList()
+            var limit = PLAYER_LIMIT
+            var cnt = 0
+
+            while(limit>0)
+            {
+                var tmpSocket:BluetoothSocket = serverSocket.accept()
+                sockets.add(tmpSocket)
+                connectedInfo.text = resources.getString(R.string.connectedPlayersInfo) + cnt.toString()
+
+
+                //getting data from client on tmpSocket
+                //add received data to string list and notify
+
+                cnt++
+                limit--
+            }
+
+        }
+
+
+
     }
 }
