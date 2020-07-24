3741ae986f73a6418e63521cf66f30dbe567c80d
creativecbr <lesniakcbr@gmail.com>
2020-07-24 15:29:35 +0200

    Add: - not working service for scaning devices - broadcast receivers for pairing devices Modified: - improvment of discoverable service button on server on broadcast receiver Need: -DeviceListAdapter

--- app/src/main/java/lesniewski/pawel/uwd_android_studio/JoinToRoom.kt
+++ app/src/main/java/lesniewski/pawel/uwd_android_studio/JoinToRoom.kt
@@ -1,11 +1,85 @@
 package lesniewski.pawel.uwd_android_studio
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class JoinToRoom : AppCompatActivity() {
+
+    var TAG:String="aplka"
+    lateinit var scanButton: Button
+    lateinit var lvDevicesList: ListView
+    lateinit var connectStatus: TextView
+    var BTDevices = ArrayList<BluetoothDevice>()
+    var arrayAdapter: ArrayAdapter<BluetoothDevice>? = null
+    var bAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_to_room)
+
+        scanButton = findViewById(R.id.scanButton)
+        lvDevicesList = findViewById(R.id.foundDevicesList)
+        connectStatus = findViewById(R.id.connectStatus)
+
+        arrayAdapter =  ArrayAdapter<BluetoothDevice>(
+            this,
+            android.R.layout.simple_list_item_1,
+            BTDevices
+        )
+        lvDevicesList.adapter = arrayAdapter
+
+        scanButton.setOnClickListener {
+
+            if(bAdapter.isDiscovering)
+            {
+                bAdapter.cancelDiscovery()
+                bAdapter.startDiscovery()
+                var discoverIntentFilter:IntentFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+                registerReceiver(scanReceiver, discoverIntentFilter)
+            }
+            if(!bAdapter.isDiscovering)
+            {
+                bAdapter.startDiscovery()
+                var discoverIntentFilter:IntentFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+                registerReceiver(scanReceiver, discoverIntentFilter)
+            }
+        }
+
     }
+
+    val scanReceiver = object: BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        override fun onReceive(ctx: Context?, intent: Intent?) {
+            var action: String? = intent?.action
+            Log.d(TAG, "action found")
+            if(BluetoothDevice.ACTION_FOUND == action)
+            {
+
+                var device: BluetoothDevice? = intent?.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                if (device != null) {
+                    scanButton.text = device.name
+                    BTDevices.add(device)
+                }
+                arrayAdapter?.notifyDataSetChanged()
+            }
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/lesniewski/pawel/uwd_android_studio/ServerWaitingForPlayers.kt
+++ app/src/main/java/lesniewski/pawel/uwd_android_studio/ServerWaitingForPlayers.kt
@@ -6,9 +6,13 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothServerSocket
 import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
@@ -21,6 +25,7 @@ import kotlin.collections.ArrayList
 
 class ServerWaitingForPlayers : AppCompatActivity()
 {
+    private var TAG = "akcja!"
     private var ROOM_NAME = "---"
     private var PLAYER_LIMIT = 6
     private val APP_UUID = UUID.fromString("3b4c7719-3738-4234-94a3-22d72dbb8a74")
@@ -67,6 +72,39 @@ class ServerWaitingForPlayers : AppCompatActivity()
         }*/
     }
 
+    val scanModeReceiver = object: BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        override fun onReceive(p0: Context?, intent: Intent?) {
+            var action = intent?.action
+            if(action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED))
+            {
+                var modeValue =
+                    intent?.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR)
+
+                if(modeValue == BluetoothAdapter.SCAN_MODE_CONNECTABLE)
+                {
+                    refreshButton.text = resources.getString(R.string.refreshAgain)
+                    refreshButton.isEnabled = true
+                }
+                else if(modeValue == BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE)
+                {
+                    refreshButton.text =  resources.getString(R.string.discoverableNow)
+                    refreshButton.isEnabled = false
+                }
+                else if(modeValue == BluetoothAdapter.SCAN_MODE_NONE)
+                {
+                    refreshButton.text  = resources.getString(R.string.refreshButton)
+                    refreshButton.isEnabled = true
+                }
+                else
+                {
+                    Toast.makeText(this@ServerWaitingForPlayers, resources.getString(R.string.problemWithDiscoverability), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }
+
     private fun discoverabilityButtonService() {
 
         refreshButton.setOnClickListener{
@@ -74,6 +112,9 @@ class ServerWaitingForPlayers : AppCompatActivity()
             val intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
             intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABLE_DURATION)
             startActivityForResult(intent, REQUEST_CODE_ENABLE_DISCOVERABILTY)
+
+            val intentFilter = IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)
+            registerReceiver(scanModeReceiver, intentFilter)
 
         }
     }
@@ -87,7 +128,7 @@ class ServerWaitingForPlayers : AppCompatActivity()
                 {
                     Toast.makeText(this, "Musisz być widoczny, aby inni gracze dołączyli do ciebie.", Toast.LENGTH_LONG).show()
                 }
-                else
+              /*  else
                 {
                     refreshButton.isEnabled = false
                     refreshButton.text = resources.getString(R.string.discoverableNow)
@@ -98,6 +139,8 @@ class ServerWaitingForPlayers : AppCompatActivity()
 
                     }, DISCOVERABLE_DURATION.toLong() * 1000)
                 }
+
+               */
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
